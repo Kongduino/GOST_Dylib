643eb7480d4cdf243488e174062a63bca82a799a
Kongduino <Kongduino@gmail.com>
2024-11-17 00:27:45 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "gost_3412_2015_calc.h"
+#include "test.h"
+
+int main(int argc, char *argv[]) {
+  vect gipher_blk;
+  GOST_Kuz_Expand_Key(test_key);
+  GOST_Kuz_Encrypt(encrypt_test_string, gipher_blk);
+  GOST_Kuz_Decrypt(decrypt_test_string, gipher_blk);
+  return 0;
+}
