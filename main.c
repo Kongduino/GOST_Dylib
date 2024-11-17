1f85dbd7873da65617843eebb7d51066abc938e6
Kongduino <Kongduino@gmail.com>
2024-11-17 16:31:33 +0800

    Update to kuznechik_test

--- main.c
+++ main.c
@@ -15,5 +15,9 @@ int main(int argc, char *argv[]) {
   GOST_Kuz_Decrypt(cipher_blk, decipher_blk);
   printf("DECIPHERED\n");
   hexDump((unsigned char *)decipher_blk, 16);
+  if(memcmp(decipher_blk, encrypt_test_string, 16) == 0)
+    printf("    [√] Output is correct!\n\n");
+  else
+    printf("    [x] Output is incorrect!\n\n");
   return 0;
 }
